03a7819cf892f8dd573661b85aebdc7208c4dd6d
jozsefcsernatoni <jozsef.csernatoni@gmail.com>
2026-05-10 08:13:25 +0300

    Finished code with factories wrapped up in a module

--- custom.js
+++ custom.js
@@ -1,18 +1,128 @@
 const game=(()=>{
-let arrPlayers=[];
+
+const allHeaderInput=document.querySelectorAll(".header>input");
+const allHeaderSelectors=document.querySelectorAll(".header>select")
+
+function gameStatus(){
+    let gameOn=false;
+    const setGame=function(state){
+        gameOn=state;
+    }
+    const getGame=function(){
+        return gameOn;
+    }
+    return {setGame,getGame};
+}
+
+const gameSwitch=gameStatus();
+
+function curPlayerMan(){
+    let curPlayerIndex=1;
+    const getcurPlayerIndex=function(){
+        if(curPlayerIndex){
+            curPlayerIndex=0;
+        } else {
+            curPlayerIndex=1;
+        }
+        return curPlayerIndex;
+    };
+
+const resetcurPlayerIndex=function(){
+    curPlayerIndex=1;
+    return curPlayerIndex;
+}
+
+    return{getcurPlayerIndex,resetcurPlayerIndex};
+}
+
+const playerIndex=curPlayerMan();
+
+function playerMan(){
+    let arrPlayers=[];
+
+    function nodeValue(nodeArr){
+    let arr=[];
+    nodeArr.forEach(element => {
+        arr.push(element.value);
+    });
+    return arr;
+    }
+
+   
+
+    const setPlayers=function(nameNodeL, markerNodeL){
+        let inputs=nodeValue(nameNodeL);
+        let selectors=nodeValue(markerNodeL);
+        for(let i=0;i<inputs.length; i++){
+                let player={name:inputs[i], 
+                marker:selectors[i]
+            };
+    arrPlayers.push(player);
+    }
+ 
+    }
+    const clearPlayers=function(){
+        arrPlayers=[];
+    }
+
+    const getPlayers=function(){
+        return arrPlayers;
+    }
+   return {setPlayers,clearPlayers,getPlayers};
+}
+
+
+const playerMaker=playerMan();
+
+function boardMan(){
 let board=[
         ["","",""],
         ["","",""],
         ["","",""]
     ];
 
-const arrMarkers=["X","O"];
-let curPlayerIndex=0;
-
-const createPlayer=function (name, marker){
-    let player={name,marker};
-    arrPlayers.push(player);
+    const getBoard=function(){
+        return board;
+    }
+    const resetBoard=function(){
+        board=[
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ];
+    }
+    return {getBoard,resetBoard};
 }
+
+const boardManager=boardMan();
+
+
+
+
+const init=function(){
+    window.addEventListener("load",function(e){
+        playerMaker.setPlayers(allHeaderInput,allHeaderSelectors); 
+
+    })
+}
+
+const startGame=function(){
+    const btStart=document.querySelector("#btnstart");
+    btStart.addEventListener("click",function()
+    {
+        gameSwitch.setGame(true);
+        btStart.textContent="Restart";;
+        if(btStart.textContent==="Restart"){
+        boardManager.resetBoard();
+        playerIndex.resetcurPlayerIndex();
+        displayUnit.setDisplay("");
+        readBoard();
+        }
+    }
+);
+}
+
+
 
 
 const readBoard=function(){
@@ -20,15 +130,15 @@ const readBoard=function(){
     const virtBoard=document.querySelectorAll(".square");
     for(let i=0;i<virtBoard.length;i++){
         let [usrColumn,usrRow]=virtBoard[i].id;
-        virtBoard[i].textContent=board[usrColumn][usrRow];
+        virtBoard[i].textContent=boardManager.getBoard()[usrColumn][usrRow];
     }
 }
 
 //
 const getWin=function(){
 function getRows(){
-            for(let i=0;i<3;i++){
-                let curRow=board[i].slice(0,3);
+            for(let i=0;i<2;i++){
+                let curRow=boardManager.getBoard()[i].slice(0,3);
                 if(curRow[0]===curRow[1] && curRow[1]===curRow[2] && curRow[0]!=""){
                 return curRow[0];  
             }
@@ -36,8 +146,8 @@ function getRows(){
         }
 
         function getColumn(){
-            for(let i=0;i<3;i++){
-                let curCol = board.map(x => x[i]); 
+            for(let i=0;i<2;i++){
+                let curCol = boardManager.getBoard().map(x => x[i]); 
                 if(curCol[0]===curCol[1] && curCol[1]===curCol[2] && curCol[0]!=""){
                     return curCol[0];
                 }}
@@ -45,168 +155,105 @@ function getRows(){
         }
 
         function getDiag(){
-            if((board[0][0]===board[1][1] && board[1][1]===board[2][2] && board[0][0]!="") || (board[2][0]===board[1][1] && board[0][2]===board[1][1] && board[2][0]!=" ")){
-                return board[1][1];
+            if((boardManager.getBoard()[0][0]===boardManager.getBoard()[1][1] && boardManager.getBoard()[1][1]===boardManager.getBoard()[2][2] && boardManager.getBoard()[0][0]!="") || (boardManager.getBoard()[2][0]===boardManager.getBoard()[1][1] && boardManager.getBoard()[0][2]===boardManager.getBoard()[1][1] && boardManager.getBoard()[2][0]!=" ")){
+                return boardManager.getBoard()[1][1];
             }
         }
 
         if(getRows() || getColumn() || getDiag()){
-            arrPlayers.filter(function(el){
+            playerMaker.getPlayers().filter(function(el){
                 const winMarker=getRows() || getColumn() || getDiag();
                 if(el.marker===winMarker){
-                    console.log (el.name);
+                    displayUnit.setDisplay("And the winner is: " +el.name);
+                    gameSwitch.setGame(false);
                 }
                 
                
             });
-        } else if(board.flat().indexOf("")===-1){
-            console.log ("it's a tie");
+        } else if(boardManager.getBoard().flat().indexOf("")===-1){
+            displayUnit.setDisplay("It's a tie!");
+            gameSwitch.setGame(false);
         }
     }
 
 
-//
 
-const readCurPlayerIndex=function(){
-const tempPlayer=curPlayerIndex;
-if (curPlayerIndex){
-    curPlayerIndex=0;
-} else {
-    curPlayerIndex=1;
-}
-return tempPlayer;
-}
 
 
 const readClicks=function(){
     const divContainer=document.querySelector(".container");
-    divContainer.addEventListener("click",function(e){
+        divContainer.addEventListener("click",function(e){
         let [usrColumn,usrRow]=e.target.id;
-        if(board[usrColumn][usrRow]==""){
-            board[usrColumn][usrRow]=arrMarkers[readCurPlayerIndex()];
+        if(boardManager.getBoard()[usrColumn][usrRow]=="" && gameSwitch.getGame()){
+            boardManager.getBoard()[usrColumn][usrRow]=allHeaderSelectors[playerIndex.getcurPlayerIndex()].value;
             readBoard();
-            console.log(board);
             
             getWin();
         }
-    });
+        });
+    
 }
 
 const updateName=function(){
-    const btnEdit=document.querySelector("#btnedit");
-    const allHeader=document.querySelectorAll(".header>*");
-    const selectors=document.querySelectorAll("select");
-
-    selectors[0].addEventListener("change",function(ev){
-            if(selectors[0].selectedIndex==1){
-                selectors[1].selectedIndex=2;            }  
-                else {selectors[1].selectedIndex=1;}    
-        } 
-    );
-    selectors[1].addEventListener("change",function(ev){
-            if(selectors[1].selectedIndex==1){
-                selectors[0].selectedIndex=2;            }  
-                else {selectors[0].selectedIndex=1;}    
-        } 
-    );
+const btnEdit=document.querySelector("#btnedit");
 
 
-
-    btnEdit.addEventListener("click",function(e){
-        if(e.target.textContent==="Edit"){
-           for(let i=0;i<selectors.length;i++) 
-          {
-                for(let j=0;j<arrMarkers.length;j++){
-                    const option=document.createElement("option");
-                    option.value=arrMarkers[j];
-                    option.id=arrMarkers[j];
-                    option.textContent=arrMarkers[j];
-                    selectors[i].appendChild(option);
-                } 
-              }
-           
-
-
-
-
-
-            let temp;
-            for(let i=0; i<allHeader.length; i++){
-                if(allHeader[i].classList.contains("hide")) {
-                    allHeader[i].classList.toggle("hide");
-                    
-                } else if(!(allHeader[i].classList.contains("show"))){
-                    allHeader[i].classList.toggle("hide");
-     
-                }
-            if(allHeader[i].tagName=="DIV"){
-                temp=allHeader[i].textContent;
+btnEdit.addEventListener("click",function(e){
+    if(e.target.textContent==="Edit"){
+        for(let i=0;i<allHeaderInput.length;i++){
+            allHeaderInput[i].removeAttribute("disabled");
+            allHeaderSelectors[i].removeAttribute("disabled");
+    }
+        
+    btnEdit.textContent="Save";
+    }  else if(e.target.textContent==="Save"){
+            for(let i=0;i<allHeaderInput.length;i++){
+            allHeaderInput[i].setAttribute("disabled","");
+            allHeaderSelectors[i].setAttribute("disabled","");
+            } 
+            playerMaker.clearPlayers();       
+            playerMaker.setPlayers(allHeaderInput,allHeaderSelectors);  
+            btnEdit.textContent="Edit";
             }
-             if(allHeader[i].tagName=="INPUT"){
-                allHeader[i].value=temp ;
-
-            }
-            }
-                btnEdit.textContent="Save";
-            } else if(e.target.textContent==="Save"){
-                let inputs=[];
-                    let sel=[];
-
-                for(let i=0; i<allHeader.length; i++){
-                if(allHeader[i].classList.contains("hide")) {
-                    allHeader[i].classList.toggle("hide");
-                    
-                } else if(!(allHeader[i].classList.contains("show"))){
-                    allHeader[i].classList.toggle("hide");
-     
-                }
-            }
-
-
-                for(let i=0; i<allHeader.length; i++){
-                    
-                if(allHeader[i].tagName=="INPUT"){
-                    inputs.push(allHeader[i].value );
-                }
-                if(allHeader[i].tagName=="SELECT"){
-                    sel.push(allHeader[i].value) ;
-                }
-                
-                }
-                    if(arrPlayers.length===0){
-                        
-                        createPlayer(inputs[0],sel[0]);
-                        createPlayer(inputs[1],sel[1]);
-                    }
-                        
-                    else {
-                        arrPlayers=[];
-                        createPlayer(inputs[0],sel[0]);
-                        createPlayer(inputs[1],sel[1]);
-                    }
-                    console.log(arrPlayers);
-                }
-       
     });
+
+
+   
+    {
+        allHeaderSelectors[0].addEventListener("change",function(ev){
+            if(allHeaderSelectors[0].selectedIndex==0){
+                allHeaderSelectors[1].selectedIndex=1;            }  
+                else {allHeaderSelectors[1].selectedIndex=0;}    
+        } 
+    );}
+    allHeaderSelectors[1].addEventListener("change",function(ev){
+            if(allHeaderSelectors[1].selectedIndex==0){
+                allHeaderSelectors[0].selectedIndex=1;            }  
+                else {allHeaderSelectors[0].selectedIndex=0;}    
+        } 
+    );
+
+
+
+   
 }
 
 
+function displayMan(){
+    const display=document.querySelector(".display");
 
+    const setDisplay=function(content){
+        display.textContent=content;
+    }
+    return {setDisplay};
+}
+const displayUnit=displayMan();
+
+
+init();
 updateName();
+startGame();
 readBoard();
 readClicks();
-
-//console.log(player1);
-
-// user input to create users
-let usrPlayer1="";
-
-//console.log(usrPlayer1Mark());
-
-// show the board
-//console.log(readBoard());
-
-// make move
-
 
 })();
